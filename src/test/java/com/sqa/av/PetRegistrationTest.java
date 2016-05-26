9f18250fa376a34d55dfbaf24e5663a326178083
Arunkumar Venkatraman <arunkumar.venkatraman@gmail.com>
2016-05-25 20:42:21 -0700

    Create PetRegistration test class

---
+++ src/test/java/com/sqa/av/PetRegistrationTest.java
@@ -0,0 +1,36 @@
+/**
+ *   File Name: PetRegistrationTest.java<br>
+ *
+ *   Venkatraman, Arunkumar<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: May 25, 2016
+ *   
+ */
+
+package com.sqa.av;
+
+import org.junit.*;
+
+/**
+ * PetRegistrationTest //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author Venkatraman, Arunkumar
+ * @version 1.0.0
+ * @since 1.0
+ *
+ */
+public class PetRegistrationTest {
+	/**
+	 * Test method for {@link com.sqa.av.PetRegistration#registerPet()}.
+	 */
+	@Test
+	public void testRegisterPet() {
+		PetRegistration.registerPet();
+	}
+
+}

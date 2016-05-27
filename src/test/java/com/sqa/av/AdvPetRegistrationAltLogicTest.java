205743e563e732902c207e82f12303a3c0dce7d4
Arunkumar Venkatraman <arunkumar.venkatraman@gmail.com>
2016-05-26 21:34:30 -0700

    Create Adv Pet Reg Alt Logic Test

---
+++ src/test/java/com/sqa/av/AdvPetRegistrationAltLogicTest.java
@@ -0,0 +1,33 @@
+/**
+ *   File Name: AdvPetRegistrationAltLogic.java<br>
+ *
+ *   Venkatraman, Arunkumar<br>
+ *   Java Boot Camp Exercise<br>
+ *   Instructor: Jean-francois Nepton<br>
+ *   Created: May 26, 2016
+ *   
+ */
+
+package com.sqa.av;
+
+import org.junit.*;
+
+/**
+ * AdvPetRegistrationAltLogic //ADDD (description of class)
+ * <p>
+ * //ADDD (description of core fields)
+ * <p>
+ * //ADDD (description of core methods)
+ * 
+ * @author Venkatraman, Arunkumar
+ * @version 1.0.0
+ * @since 1.0
+ *
+ */
+public class AdvPetRegistrationAltLogicTest {
+	@Test
+	public void testRegisterPet() {
+
+		AdvPetRegAltLogic.registerPet();
+	}
+}
